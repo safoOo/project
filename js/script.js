11ead3adb1c486a1e43224d6c65f10b9523c2992
beko09 <abobakerhlal09@gmail.com>
2019-11-06 14:41:53 +0300

    style header by flexbox

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-$(document).ready(function(){
-    $('.menu-toggle').on('click',function(){
+$(document).ready(function () {
+    //  show and hide nav link
+    $('.menu-toggle').on('click', function () {
         $('.nav').toggleClass('showing');
-        $('.nav ul').toggleClass('showing');
+    });
+    //  if you want show and hide element don't use one fumction
+    //  show and hide dropmenu
+    $('.login').on('click', function () {
+        $('.login .dropdown').toggleClass('showing');
     });
 });
